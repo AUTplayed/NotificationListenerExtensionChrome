ff04721260b18f5a90af503a3cb79964752011ea
AUTplayed <fips.hem@gmail.com>
2019-05-01 02:30:15 +0200

    added clear action

--- popup.js
+++ popup.js
@@ -11,10 +11,22 @@ async function open() {
 	ruler.style = "display:none";
 	content.style.width = contentMaxWidth;
 	content.innerHTML = html;
+	var clears = document.getElementsByClassName("clear");
+	for (var i = 0; i < clears.length; i++) {
+		clears[0].onclick = clearNotif;
+	}
 }
 open();
 
 var contentMaxWidth = 0;
+
+async function clearNotif(ev) {
+	var key = ev.target.getAttribute("key");
+	var options = await chrome.extension.getBackgroundPage().getStorageDataAsync();
+	fetch(options.url + "/action" + "?secret=" + options.secret, {
+		method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ clear: [key] })
+	}).then(console.log("sent action"));
+}
 
 function buildEntry(data) {
 	var dateFormatted = new Date(data.time).toTimeString().split(" ")[0];
@@ -22,7 +34,7 @@ function buildEntry(data) {
 	calcMaxWidth(data.content);
 	calcMaxWidth(data.source + " " + dateFormatted);
 	return `<div style="display:flex;flex-direction:column">
-			<div>${data.title}</div>
+			<div style="display:flex;justify-content:space-between;"><span>${data.title}</span><span style="cursor:pointer;" class="clear" key="${data.key}">✖</span></div>
 			<div>${data.content}</div>
 			<div style="display:flex;justify-content:space-between;"><span>${data.source}</span><span>${dateFormatted}</span></div>
 		</div>`;
