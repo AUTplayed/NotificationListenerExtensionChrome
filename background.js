e534029480fd3565c92a8e7ed97b2329aac33225
Philipp Hemmelmayr <philipp.hemmelmayr@dynatrace.com>
2019-08-26 08:07:37 +0200

    added some basic error handling

--- background.js
+++ background.js
@@ -4,9 +4,17 @@ setInterval(() => {
 
 async function work() {
 	var options = await getStorageDataAsync();
-	var data = await fetch(options.url + "?secret=" + options.secret);
-	var json = await data.json();
+	var json;
+	try {
+		var data = await fetch(options.url + "?secret=" + options.secret);
+		json = await data.json();
+	} catch(e) {
+		chrome.browserAction.setBadgeBackgroundColor({color:"#f00"});
+		chrome.browserAction.setBadgeText({ "text": "x" });
+		return [];
+	}
 	var badgeText = json.length > 0 ? json.length + "" : "";
+	chrome.browserAction.setBadgeBackgroundColor({color:[51, 103, 214, 255]});
 	chrome.browserAction.setBadgeText({ "text": badgeText });
 	return json;
 }
